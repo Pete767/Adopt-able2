283acdf62ddde30b80a8db2ea958f618b3e08650
Pete Gonzales <petegonzales@Petes-Air.lan>
2024-01-05 01:13:30 -0600

    version 2

---
+++ functions/petfinderProxy.js
@@ -0,0 +1,33 @@
+const fetch = require('node-fetch');
+
+exports.handler = async function (event, context) {
+  try {
+    const petfinderApiKey = process.env.REACT_APP_PETFINDER_API_KEY;
+
+    if (!petfinderApiKey) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ error: 'Petfinder API key is missing.' }),
+      };
+    }
+
+    const response = await fetch('https://api.petfinder.com/v2/types', {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${petfinderApiKey}`,
+      },
+    });
+
+    const data = await response.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Internal Server Error' }),
+    };
+  }
+};
